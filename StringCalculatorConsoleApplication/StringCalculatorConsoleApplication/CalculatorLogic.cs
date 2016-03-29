b6e92ae4a4d470d37107ba775391a7cbac67ec0b
DanilTol <d.a.tolmachov@gmail.com>
2016-03-29 20:01:27 +0300

    First two tasks complete/

--- StringCalculatorConsoleApplication/StringCalculatorConsoleApplication/CalculatorLogic.cs
+++ StringCalculatorConsoleApplication/StringCalculatorConsoleApplication/CalculatorLogic.cs
@@ -1,7 +1,16 @@
-﻿namespace StringCalculatorConsoleApplication
+﻿using System;
+using System.Linq;
+
+namespace StringCalculatorConsoleApplication
 {
     public class CalculatorLogic
     {
-         
+        public static int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            return numbers.Split(',').Select(strNum => Convert.ToInt32(strNum)).Sum();
+        }
     }
 }
---
+++ StringCalculatorConsoleApplication/StringCalculatorUnitTestProject/CalculatorLogicClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorUnitTestProject
+{
+    [TestClass]
+    public class CalculatorLogicClass
+    {
+        [TestClass]
+        public class AddMethod
+        {
+            [TestMethod]
+            public void EmptyInput_OutputZero()
+            {
+                //arrange
+                string input = string.Empty;
+                //act
+                int ans = StringCalculatorConsoleApplication.CalculatorLogic.Add(input);
+                //assert
+                Assert.AreEqual(0,ans,"Input is empty string");
+            }
+
+            [TestMethod]
+            public void OneArgInput_OutputInputArg()
+            {
+                //arrange
+                string input = "2";
+                //act
+                int ans = StringCalculatorConsoleApplication.CalculatorLogic.Add(input);
+                //assert
+                Assert.AreEqual(2, ans, "Input one arg");
+            }
+
+            [TestMethod]
+            public void TwoArgsInput_OutputSumOfArgs()
+            {
+                //arrange
+                string input = "1,2";
+                //act
+                int ans = StringCalculatorConsoleApplication.CalculatorLogic.Add(input);
+                //assert
+                Assert.AreEqual(3, ans, "Input two arg");
+            }
+
+            [TestMethod]
+            public void FourArgsInput_OutputSumOfArgs()
+            {
+                //arrange
+                string input = "1,2,3,4";
+                //act
+                int ans = StringCalculatorConsoleApplication.CalculatorLogic.Add(input);
+                //assert
+                Assert.AreEqual(10, ans, "Input two arg");
+            }
+        }
+    }
+}
